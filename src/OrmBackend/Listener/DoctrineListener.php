faf6cfc4d40e956bbce059e6b1cd478ee8bc8904
Vitaliy Kovalenko <vitalykolsky@gmail.com>
2020-09-06 18:15:51 +0300

    Demo mode fix

--- src/OrmBackend/Listener/DoctrineListener.php
+++ src/OrmBackend/Listener/DoctrineListener.php
@@ -4,6 +4,7 @@ namespace OrmBackend\Listener;
 use OrmBackend\SoftDeleteable;
 use Doctrine\ORM\Event\PreFlushEventArgs;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 /**
  * 
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Auth;
 class DoctrineListener
 {
     public function preFlush(PreFlushEventArgs $event) {
+        if (env('DEMO', false) && Auth::id() != 1) {
+            throw ValidationException::withMessages(['Data modification is not available in demo mode. This interrupt is triggered from the Doctrine Session PreFlush event.']);
+        }
+        
         $em = $event->getEntityManager();
         
         foreach ($em->getUnitOfWork()->getScheduledEntityDeletions() as $object) {
